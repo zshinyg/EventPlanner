fc079be2fb237730b1529576db43967c0450dda2
Jamison Walrod <35571086+Prototroid@users.noreply.github.com>
2018-01-31 14:35:28 -0600

    Update adminMode.js

--- adminMode.js
+++ adminMode.js
@@ -1,41 +1,41 @@
 // Jamison Walrod
 // EECS 448
 // Project 1
-// 29 January 2018
+// 31 January 2018
 //
 // Code for checkbox used in html was found on StackOverflow: https://stackoverflow.com/questions/28372466/how-to-call-a-function-from-checkbox-onclick-or-onchange
 // String to array: https://stackoverflow.com/questions/19418244/elegant-way-to-convert-string-of-array-of-arrays-into-a-javascript-array-of-arra
 // Try and catch: https://www.w3schools.com/js/js_errors.asp
 // Checking if parameter is an array: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
 // Check to see if string includes a character:	https://www.w3schools.com/jsref/jsref_includes.asp
-// Using reduce to flatten a nested array: https://stackoverflow.com/questions/27266550/how-to-flatten-nested-array-in-javascript
+// 
 // 
 //
 
 main = function() {
 
-	chalk.print("Please use your administrative login.");
+	//chalk.print("Please use your administrative login.");
 	chalk.newline();
 }
 
 
 //
-// Creates an array and calls flatten function; catches errors
+// Takes in the username and password of an administrator 
 function adminLogin()
 {
 	try
 	{
 		//// Heading ////
 		chalk.newline();
-		chalk.print("Please use your administrative login.");
-		chalk.newline();
+		//chalk.print("Please use your administrative login.");
+		//chalk.newline();
 		
 		//// Enter username ////
 		chalk.print("Username: ");
 		let u = chalk.entrybox();
 		u.then(function(val)
 		{
-			console.log(val);
+			console.log("Username: ", val);
 		});
 		
 		//// Enter password ////
@@ -43,38 +43,76 @@ function adminLogin()
 		let p = chalk.entrybox();
 		p.then(function(val)
 		{
-			console.log(val);
+			console.log("Password: ", val);
 		});
 		
 		Promise.all([u,p]).then(function(a)
 		{
-			let arr;
+			
+			console.log("Inside promise");
 			
 			if(a[0].includes(" "))
 			{
+				console.log(a);
+				console.log("ERROR: Invalid username!");
+				//chalk.println("");
+				chalk.println("Invalid username! Cannot contain spaces!");
 				throw e;
+				
+			}
+			else if(a[1].includes(" "))
+			{
+				console.log(a);
+				console.log("ERROR: Invalid password!");
+				//chalk.println("");
+				chalk.println("Invalid password! Cannot contain spaces!");
+				throw e;
+			}
+			else if(adminCompare(a) != true)
+			{
+				console.log(a);
+				console.log("ERROR: No login found!");
+				chalk.println("");
+				chalk.println("No login found! Please try again.");
+				adminLogin();
 			}
 			else
 			{
-				console.log(arr);
+				console.log("Success!");
+				chalk.println("");
+				chalk.println("Welcome!");
+				//-> eventFunction();
 			}
-			
-			//let flat = flatten(arr);
-			//showArray(flat);
 			
 		}).catch(function(e)
 		{
-			console.log(p);
-			console.log("ERROR IN PROMISE: Invalid input!");
-			chalk.println("OOPS!");
-			chalk.println("Invalid input in promise!");
+			console.log(u);
+			console.log("ERROR: Invalid login!");
+			//chalk.println("");
+			chalk.println("Invalid login! Please try again.");
+			adminLogin();
 		});
 	}
 	catch(err)
 	{
 		console.log("ERROR: Invalid input!");
-		chalk.println("OOPS!");
-		chalk.println("Invalid input!");
+		chalk.println(" ");
+		chalk.println("ERROR");
 	}
 }
 
+//
+// Compares user login creditentials to  to see if they are in the system.
+function adminCompare(a)
+{
+	let arr = ["admin","1234"];
+	console.log(a);
+	if(JSON.stringify(a) == JSON.stringify(arr))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
