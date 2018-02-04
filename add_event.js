85a0cb9593af516b880d7be4e59aa1e46e99e991
Zachary McGrath <35537334+zmcgrath96@users.noreply.github.com>
2018-02-04 12:33:57 -0600

    Pulling event data
    got the data, need to toggle times am/pm to save in 24 hour format

--- add_event.js
+++ add_event.js
@@ -14,9 +14,12 @@
  * select box with the correct number of options.
  */
 
+// Global variable for saving time in 24hr time format or not
+ var clock24 = false;
+
 function timeSwitcher(){
     console.log("timeSwitcher Ran");
-    
+
     var checkBox = document.getElementById("timeCheckBox");
     var el1 = "startHour";
     var el2 = "endHour";
@@ -27,14 +30,16 @@ function timeSwitcher(){
         unpopulateHour(el2);
         populateHours(23,0,el2);
         removeAMPM();
+        clock24 = true;
 
     } else {
-        
+
         unpopulateHour(el1);
         populateHours(12,1,el1);
         unpopulateHour(el2);
         populateHours(12,1,el2);
         addAMPM();
+        clock24 = false;
     }
 }
 
@@ -63,14 +68,14 @@ function addAMPM(){
  */
 
  function populateHours(mode,i,id) {
-     
+
      var docfrag = document.createDocumentFragment();
 
      for (i; i <=mode; ++i)
      {
           docfrag.appendChild(new Option(i, i));
      }
-     
+
      var select = document.getElementById(id);
      select.appendChild(docfrag);
  }
@@ -78,7 +83,7 @@ function addAMPM(){
 
 
  /**
-  * Remove 24 Hour mode 
+  * Remove 24 Hour mode
   */
 
 function unpopulateHour(id){
@@ -93,6 +98,57 @@ function unpopulateHour(id){
 
 
 
+
+
+function submitVals()  {
+  var eventTitle = document.getElementById("event").value;
+  var date = document.getElementById("date").value;
+
+  // var date is in form yyyy-mm-dd
+  //                     0123456789
+  // .substr(start,length)
+  var dd = date.substr(8,2);
+  var mm = date.substr(5,2);
+  var yyyy = date.substr(0,4);
+
+  var shour = document.getElementById("startHour").value;
+  var smin = document.getElementById("startMinutes").value;
+  var lhour = document.getElementById("endHour").value;
+  var lmin = document.getElementById("endMinutes").value;
+
+  var ampm1 = document.getElementById("ampm");
+  console.log(ampm1);
+  var ampm2 = document.getElementById("ampm2");
+  console.log(ampm2);
+
+  // TODO For the hours, see if 24/12 hours are toggled.  If 12, see if it is am or pm
+  //   FOR STORING THE TIME IN 24 HOUR MODE
+  //  ALL TIMES OF EVENTS ARE STORED THIS WAY 
+  if (!clock24){
+     // See if am/pm selected for both cases
+     // If it is pm, add 12 to the value
+     if ((ampm1 == "PM") && (shour > 12)){
+       shour = shour+12;
+     }
+     if ((ampm2 == "PM") && (lhour > 12)){
+       lhour = lhour+12;
+     }
+     if ((ampm1 == "AM") && (shour == 12))
+     {
+       shour = 0;
+     }
+     if ((ampm2 == "AM") && (lhour ==12)){
+       lhour =0;
+     }
+
+  }
+
+  // TEST IN CONSOLE
+  console.log("Event name: "+ eventTitle);
+  console.log("Date: " + dd + "/" + mm + "/" + yyyy);
+  console.log("Start time: "+shour + ":"+smin);
+  console.log("End time: " + lhour + ":" + lmin);
+}
 
 
 
--- meeting.js
+++ meeting.js
@@ -8,13 +8,17 @@
 //          TITLE
 // INTEGER VARIABLES:
 //          MONTH, DAY, YEAR, HOUR, MIN
-function meeting (title, month, day, year, hour, min) {
+//          SMIN AND SHOUR ARE START HOUR MIN AND HOUR
+//          L IS LAST 
+function meeting (title, month, day, year, shour, smin, lhour, lmin) {
   this.title = title;
   this.month = month;
   this.day = day;
   this.year = year;
-  this.hour = hour;
-  this.min = min;
+  this.shour = shour;
+  this.smin = smin;
+  this.lhour = lhour;
+  this.lmin = lmin;
   this.list = new linkedlist();
 }
 
