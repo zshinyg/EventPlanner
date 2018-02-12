b5126df0e9eab24b559dad8fd41a929ac575679f
Jamison Walrod <35571086+Prototroid@users.noreply.github.com>
2018-02-12 10:35:57 -0600

    Added @method to functions

--- user.js
+++ user.js
@@ -3,17 +3,19 @@
  * @class   user
  */
 
+/** USER.JS
+ *
+ * JavaScirpt file that accompanies user.html
+ *
+ * Has the user select from a drop down menu of people
+ * If their name is not listed, user has the option to add their name
+ * Clicks continue to move on to add availability
+*/ 
 
-
-
-// USER.JS
-//
-// JavaScirpt file that accompanies user.html
-//
-// Has the user select from a drop down menu of people
-// If their name is not listed, user has the option to add their name
-// Clicks continue to move on to add availability
-
+/**
+ * @method submitName
+ * Takes in user input for the username and submits it
+ */
 function submitName()  {
     var firstName = document.getElementById("userFirstName").value;
     var lastName = document.getElementById("userLastName").value;
@@ -38,6 +40,11 @@ function submitName()  {
 
 }
 
+/**
+ * @method populateUserSelect
+ * @param {string} id
+ * Populates the usernames so that the user can select one
+ */
 function populateUserSelect(id) {
 
     window.masterUser = populateUser("masterUser");
@@ -47,7 +54,9 @@ function populateUserSelect(id) {
     window.masterUser.printAll();
       for(var i = 0; i < masterUser.size; i++){
 
-          //Get the node at index i from masterUser
+          /**
+           * Get the node at index i from masterUser
+           */
 
           var node = masterUser.returnAt(i);
           var name = node.data.firstName  + ' ' + node.data.lastName;
@@ -64,7 +73,10 @@ function populateUserSelect(id) {
 
 }
 
-
+/**
+ * @method unpopulateNames
+ * Removes the usernames when page is reloaded
+ */
 function unpopulateNames(){
 
     var names = document.getElementById("userName");
@@ -75,15 +87,13 @@ function unpopulateNames(){
 
 }
 
-
+/**
+ * @method addAvailability
+ * Takes the selected username and carries it over from user.html to user_availability.html
+ */
 function addAvailability(){
 
   var name = document.getElementById("userName").value;
   sessionStorage.setItem("userName", name);
 
-}
-
-
-function nextPage() {
-    
 }
